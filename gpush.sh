e02ef7a9bd28dfc74875e87268c0c73267dd39af
mmark0v <malin_id@yahoo.com>
2019-07-07 00:48:34 +0100

    Directory Update

--- gpush.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-user="mmark0v"
-pass=""
-repo="https://github.com/mmark0v/ELS"
-git config --global credential.helper "cache --timeout 7200"
-git pull $repo
-git add .
-git commit -m "Directory Update"
-git push origin master
